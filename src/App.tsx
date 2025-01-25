a5fc804bcbbe315482d0e99160642d5ce85608c4
EthsMC <ethankeating1313@gmail.com>
2025-01-25 18:41:19 -0500

    Vercel being strict about unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./components/Navbar";
 import Hero from "./components/Hero";
-import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
